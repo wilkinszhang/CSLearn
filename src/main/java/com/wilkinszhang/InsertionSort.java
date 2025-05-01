9afbd81c9a197bcb7be70055c816322ab8a56500
wilkinszhang <a1ex2658536235@gmail.com>
2025-05-01 21:46:46 +0800

    调整代码路径问题

---
+++ src/main/java/com/wilkinszhang/InsertionSort.java
@@ -0,0 +1,48 @@
+package com.wilkinszhang;
+
+public class InsertionSort {
+    public static int insertionSortWithComparisonCount(int[] arr) {
+        int comparisonCount = 0; // 用于记录比较次数
+
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            // 比较当前元素与已排序部分的元素，并移动较大的元素
+            while (j >= 0) {
+                comparisonCount++; // 每次比较都记录
+                if (arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                } else {
+                    break; // 找到了合适的插入位置
+                }
+            }
+
+            arr[j + 1] = key; // 插入元素
+        }
+
+        return comparisonCount;
+    }
+
+    public static void main(String[] args) {
+        // 示例输入序列
+        int[] arr1 = {32,4,9,3,1};
+        int comparisons1 = insertionSortWithComparisonCount(arr1);
+
+        System.out.println("排序后的数组:");
+        for (int num : arr1) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\n比较次数: " + comparisons1);
+
+        // 测试更多样例
+        int[] arr2 = {1, 2, 3, 4, 5}; // 已排序的情况
+        int comparisons2 = insertionSortWithComparisonCount(arr2);
+        System.out.println("\n已排序数组的比较次数: " + comparisons2);
+
+        int[] arr3 = {5, 4, 3, 2, 1}; // 逆序的情况
+        int comparisons3 = insertionSortWithComparisonCount(arr3);
+        System.out.println("逆序数组的比较次数: " + comparisons3);
+    }
+}
---
+++ src/main/java/com/wilkinszhang/MaxStack.java
@@ -0,0 +1,53 @@
+package com.wilkinszhang;
+
+import java.util.Stack;
+
+public class MaxStack {
+    private Stack<Integer> stack; // 主栈
+    private int max; // 当前最大值
+
+    public MaxStack() {
+        stack = new Stack<>();
+        max = Integer.MIN_VALUE; // 初始最大值为最小整数
+    }
+
+    // 入栈操作
+    public void push(int x) {
+        // 如果当前值大于或等于最大值，将当前最大值存储到栈中，并更新最大值
+        if (x >= max) {
+            stack.push(max); // 存储旧的最大值
+            max = x; // 更新最大值
+        }
+        stack.push(x); // 将新值入栈
+    }
+
+    // 出栈操作
+    public int pop() {
+        int top = stack.pop(); // 弹出栈顶元素
+        if (top == max) { // 如果栈顶是最大值
+            max = stack.pop(); // 恢复之前的最大值
+        }
+        return top;
+    }
+
+    // 获取栈的最大值
+    public int getMax() {
+        return max; // 最大值存储在变量 max 中
+    }
+
+    public static void main(String[] args) {
+        MaxStack maxStack = new MaxStack();
+        maxStack.push(5);
+        maxStack.push(1);
+        maxStack.push(5);
+        System.out.println(maxStack.getMax()); // 输出: 5
+        System.out.println(maxStack.pop());    // 输出: 5
+        System.out.println(maxStack.getMax()); // 输出: 5
+        maxStack.push(7);
+        maxStack.push(7);
+        maxStack.push(8);
+        System.out.println(maxStack.getMax()); // 输出: 8
+        System.out.println(maxStack.pop());    // 输出: 8
+        System.out.println(maxStack.getMax()); // 输出: 7
+    }
+}
